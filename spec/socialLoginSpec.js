1b90cf5ef46967236e94831c91244019f12e7a40
hector <hector.ortega>
2016-09-08 19:13:30 +0200

    Some cleanup

--- spec/socialLoginSpec.js
+++ spec/socialLoginSpec.js
@@ -1,11 +1,9 @@
 var webdriver = require('selenium-webdriver');
 var logging = webdriver.logging
 var test = require('selenium-webdriver/testing');
-var By = webdriver.By;
 var Capabilities = webdriver.Capabilities;
 var util = require('util')
 var assert = require('assert');
-var until = webdriver.until;
 
 var PropertiesReader = require('properties-reader');
 var ManageLoginPage = require('../lib/page-objects/manage-login-page');
@@ -21,7 +19,12 @@ describe('Google login', function() {
     let browser;
     var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
     var properties = PropertiesReader(homeDir + '/testing-exercise.properties');
-   
+    var manageLoginPage;
+    var googleConsentPage;
+    var manageSocialConnectionsPage;
+    var callbackPage;
+
+
 
     test.beforeEach(function(done) {
         var prefs = new logging.Preferences();
@@ -30,6 +33,12 @@ describe('Google login', function() {
         var caps = Capabilities.chrome();
         caps.setLoggingPrefs(prefs);
         browser = new webdriver.Builder().withCapabilities(caps).build();
+
+        manageLoginPage = new ManageLoginPage(browser);
+        googleConsentPage = new GoogleConsentPage(browser);
+        manageSocialConnectionsPage = new ManageSocialConnectionsPage(browser);
+        callbackPage = new CallbackPage(browser);
+
         done();
     });
 
@@ -43,23 +52,18 @@ describe('Google login', function() {
 
     test.it('log in to Manage Console -> Hit Try button -> Hit Allow in Google consent page -> Callback page is displayed', function(done) {
         this.timeout(mochaTimeOut);
-        var manageLoginPage = new ManageLoginPage(browser);
-        var googleConsentPage = new GoogleConsentPage(browser);
-        var manageSocialConnectionsPage = new ManageSocialConnectionsPage(browser);
-        var callbackPage = new CallbackPage(browser);
 
         manageLoginPage.visit();
         manageLoginPage.loginWithGoogle(properties.get("manage.user"), properties.get("manage.password"));
 
         manageSocialConnectionsPage.visit();
         manageSocialConnectionsPage.tryGoogle()
+
         googleConsentPage.allow();
+        
         callbackPage.getHeaderTitle().then((title) => {
             assert.equal(title, 'It Works!', "Expected callback page title: 'It works!', but got " + title);
             browser.quit().then(done);
         });
-
     });
-
-
 });
